79249b9baa78c3bd1d726e34580b5bd11dd48288
yazeed alshami <alshamiyazeed@gmail.com>
2021-07-27 23:52:01 +0300

    finish lab03

--- horned-animals/src/App.js
+++ horned-animals/src/App.js
@@ -2,22 +2,56 @@ import React from 'react';
 import Main from './components/Main';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import SelectedBeast from './components/SelectedBeasts';
+import HornedBeasts from './components/HornedBeasts'
 
 class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state={
+      myData : HornedBeasts,
+      show : false ,
+      selectedAnimal:{}
+    };
+  }
+    handleTheModel=(title)=>{
+      let foundObj = HornedBeasts.find(element => {      
+        if (element.title === title) {
+                  return element;
+                  
+                }})
+                this.setState({
+                  show : true,
+                  selectedAnimal:foundObj
+                })
+              }
+            
+              handleClose=()=>{
+                this.setState({
+                  show : false 
+                })
+              };
 
   render() {
     return (
-      <div>
-        {/* We are creating a new instances of the Main class component */}
-        <Header/>
-        <Main />
-        <Footer />
-        
+      <>
+      <Header/>
+      <SelectedBeast
+      selectedAnimal={this.state.selectedAnimal}
+      myData={this.state.myData}
+      show ={this.state.show}
+      handleClose={this.handleClose}
+      />
+      <Main
+      myData={this.state.myData}
+      handleTheModel={this.handleTheModel}
+      
+      />
+      <Footer/>
+      </>
+    )
+  };
+};
 
-      </div>
-    );
-  }
-}
 
-// we exported our App component to be visible for other components to use
 export default App;
--- horned-animals/src/components/Main.js
+++ horned-animals/src/components/Main.js
@@ -1,29 +1,32 @@
 import React from 'react';
-import HornedAnimals from './HornedAnimals.json';
 import HornedBeasts from './HornedBeasts';
+import CardColumns from 'react-bootstrap/CardColumns';
 
 
-class Main extends React.Component{
+
+class Main extends React.Component {
+
+   
+    
   render(){
-    return(
-        <>
-        {HornedAnimals.map((item,index)=>{
-            return(
-        <HornedBeasts
-        title={item.title}
-        image_url={item.image_url}
-        description={item.description}
-        key={index}
-        />
 
-        )
-        
-    })}
-    </>
+  return(
 
-    )
+ <CardColumns>
+      {this.props.myData.map((animal)=>{
+          return(
+          <HornedBeasts
+         title={animal.title}
+         imageUrl={animal.image_url}
+         description={animal.description}
+         handleTheModel={this.props.handleTheModel}
+         />
+          )
+      })}
+  </CardColumns>
+          
+      )
   }
 }
-
 
 export default Main;
