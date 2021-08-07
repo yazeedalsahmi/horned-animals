8f6152a141034ddc1b6796c09e96f1229f422a8b
yazeed alshami <alshamiyazeed@gmail.com>
2021-08-07 10:43:18 +0300

    finish lab04

--- horned-animals/src/components/HornedBeasts.js
+++ horned-animals/src/components/HornedBeasts.js
@@ -5,43 +5,42 @@ import Button from 'react-bootstrap/Button';
 
 
 
-class HornedBeasts extends React.Component{
+class HornedBeasts extends React.Component {
 
-    constructor(props){
-      super(props)
-      this.state={
-        vote :0,
-        
+  constructor(props) {
+    super(props)
+    this.state = {
+      vote: 0,
 
-      }
+
     }
-   
-    UserClick=()=>{
-      this.setState({
-        vote : this.state.vote +1  ,
-      })
-    }
-    handleTheModelOnHornedBeasts= () => {
-      this.props.handleTheModel(this.props.title);
-    }
-  render(){
-    return(
-      <div className='cards'>
-    <Card style={{ width: '18rem' }} onClick={this.handleTheModelOnHornedBeasts}>
-    <Card.Img variant="top"  src={this.props.image_url} title={this.props.title} />
-    <Card.Body>
-      <Card.Title>{this.props.title}</Card.Title>
-      <Card.Text>
-      {this.props.description}
-      </Card.Text>
-    </Card.Body>
-    <Card.Footer>
-    <Button variant="primary" onClick ={this.UserClick}>❤️ </Button>
-      <small className="text-muted">   {this.state.vote} </small>
-    </Card.Footer>
-  </Card>
-  </div>
-   ) 
+  }
+  UserClick = () => {
+    this.setState({
+      vote: this.state.vote + 1,
+    })
+  }
+  handleUserClick = () => {
+    this.props.selected(this.props.title);
+  }
+  render() {
+    return (
+      <div className='cards' onClick={this.handleUserClick}>
+        <Card style={{ width: '18rem' }} >
+          <Card.Img variant="top" src={this.props.imageUrl} title={this.props.title} />
+          <Card.Body>
+            <Card.Title>{this.props.title}</Card.Title>
+            <Card.Text>
+              {this.props.description}
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <Button variant="primary" onClick={this.UserClick}>❤️ </Button>
+            <small className="text-muted">   {this.state.vote} </small>
+          </Card.Footer>
+        </Card>
+      </div>
+    )
 
   }
 }
